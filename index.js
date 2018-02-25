cd5eb7acdd62462c3487150a3ac85ed26f09ee89
wendy yu <yu.wendyw@gmail.com>
2018-02-25 20:50:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return recipes
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  recipes[key] = value
-  return recipes
-}
+//function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  //recipes[key] = value
+  //return recipes
+//}
+
+function deleteFromObjectByKey(object, key) {
+  delete recipes[key]
+} return recipes
