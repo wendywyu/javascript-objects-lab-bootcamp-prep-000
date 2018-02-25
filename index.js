fcfad14bf4be33d45c732845beeb73d8bfce48d1
wendy yu <yu.wendyw@gmail.com>
2018-02-25 21:26:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object, key) {
-  delete object[key]
+  return Object.assign({}, object,{[key]})
   return object
 }
 
