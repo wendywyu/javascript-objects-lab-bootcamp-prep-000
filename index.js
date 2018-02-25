7f705b086d3ab17c1e9ffe03d2d643105a2f00fc
wendy yu <yu.wendyw@gmail.com>
2018-02-25 21:09:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var recipes = { };
   //return recipes
 //}
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  recipes[key] = value
-  destructivelyUpdateObjectWithKeyAndValue(recipes, key, value)
-  return recipes
-}
+//function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  //recipes[key] = value
+  //destructivelyUpdateObjectWithKeyAndValue(recipes, key, value)
+  //return recipes
+//}
 //function deleteFromObjectByKey(object, key) {
   //delete recipes[key]
   //return recipes
